5bc4ee628d95de026531cc088c01e1b8dbe54182
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-05-22 16:50:01 +0500

    feat(database): добавил фукнцию get_db (Dependency для роутеров)

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from os import getenv
 
 from sqlalchemy import URL
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
 
@@ -19,6 +19,13 @@ engine = create_async_engine(DATABASE_URL)
 
 # 2. Сессии
 AsyncSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+# 3. Dependency для роутеров
+async def get_db() -> AsyncSession:
+    """Dependency для роутеров. Создаёт сессию БД на время запроса и закрывает после."""
+    async with AsyncSessionLocal() as session:
+        yield session  # именно yield, а не return - FastAPI автоматически закроет сессию сам
+
 
 class Base(DeclarativeBase):
     pass
