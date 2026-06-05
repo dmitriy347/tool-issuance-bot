41153227c77c77d536559b3e6c3b3e38768e2631
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-06-05 16:22:07 +0500

    feat(services/test_excel_parser.py): добавил тесты для test_parse_inventory

--- tests/test_excel_parser.py
+++ tests/test_excel_parser.py
@@ -1,10 +1,10 @@
 from datetime import date
 from pathlib import Path
 
-from services.excel_parser import parse_employee_directory
+from services.excel_parser import parse_employee_directory, parse_inventory
 
 
-def test_employee_directory():
+def test_parse_employee_directory():
     """Тестирование функции parse_employee_directory на корректность парсинга данных из Excel-файла."""
     # Путь к тестовому Excel-файлу с данными сотрудников
     file_path = Path(__file__).parent / "fixtures" / "directory_employees.xlsx"
@@ -20,3 +20,41 @@ def test_employee_directory():
         "contract_number": "1-aa",
         "contract_date": date(2024, 1, 10),
     }
+
+
+def test_parse_employee_directory_empty():
+    """Тестирование функции parse_employee_directory на корректность парсинга данных из пустого Excel-файла"""
+    file_path = Path(__file__).parent / "fixtures" / "directory_employees_1.xlsx"
+    result = parse_employee_directory(file_path)
+    assert len(result) == 0
+    assert result == []
+
+
+def test_parse_inventory():
+    """Тестирование функции parse_inventory_directory на корректность парсинга данных из Excel-файла."""
+    # Путь к тестовому Excel-файлу с данными инвентаря
+    file_path = Path(__file__).parent / "fixtures" / "1c_inventories_test.xlsx"
+
+    # Вызываем функцию парсинга и получаем результат
+    period = date(2026, 3, 1)
+    result = parse_inventory(file_path, period)
+    print(result)
+    assert len(result) == 4
+    assert result[0] == {
+        "employee_name": "Иванов Иван Иванович",
+        "tool_name": "Вороток шарнирный",
+        "tool_code": "ЦБ000001111",
+        "quantity": 2,
+        "price": 2500.16,
+        "period": period
+    }
+    assert result[1] == {
+        "employee_name": "Сергеев Сергей Сергеевич",
+        "tool_name": "Гайковерт",
+        "tool_code": "БШ-00004444",
+        "quantity": 1,
+        "price": 12000,
+        "period": period
+    }
+
+
