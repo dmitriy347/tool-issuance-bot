a7de1c911723da307d1caff063770777d7d9e25f
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-06-09 15:43:33 +0500

    feat(bot/document): добавил отправку данных в API после парсинга файлов

--- bot/handlers/document.py
+++ bot/handlers/document.py
@@ -1,3 +1,6 @@
+from os import getenv
+
+import httpx
 from aiogram import Router, Bot
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
@@ -8,21 +11,37 @@ from services.excel_parser import parse_employee_directory, parse_inventory
 
 router = Router()
 
+api_url = getenv("API_URL")
+
 @router.message(UpdateDirectory.waiting_for_file)
 async def handle_directory_file(message: Message, bot: Bot, state: FSMContext):
     """
     Обработчик загрузки файла справочника.
     Скачивает файл, парсит его, возвращает количество сотрудников в справочнике и очищает состояние.
     """
-    try:
-        file = await bot.get_file(message.document.file_id)
-        await bot.download_file(file.file_path, destination="tmp/directory.xlsx")
-        result = parse_employee_directory("tmp/directory.xlsx")  # Парсим файл справочника
-        await message.answer(f"Загружено сотрудников: {len(result)}")
-    except Exception as e:
-        await message.answer(f"Произошла ошибка при обработке файла: {str(e)}")
-    finally:
-        await state.clear()  # Очищаем состояние после завершения процесса
+    async with httpx.AsyncClient() as client:
+        try:
+            # Перед загрузкой нового файла удаляем все существующие записи о сотрудниках через API
+            response = await client.delete(f"{api_url}/api/employees/")
+            response.raise_for_status()
+
+            file = await bot.get_file(message.document.file_id)
+            await bot.download_file(file.file_path, destination="tmp/directory.xlsx")
+            result = parse_employee_directory("tmp/directory.xlsx")  # Парсим файл справочника
+
+            # Отправляем каждого сотрудника в API
+            for employee in result:
+                # Преобразуем дату в строку в формате ISO, чтобы её можно было отправить в JSON
+                employee["contract_date"] = employee["contract_date"].isoformat()
+                response = await client.post(f"{api_url}/api/employees/", json=employee)
+                response.raise_for_status()
+
+            await message.answer(f"Загружено сотрудников: {len(result)}")
+        except Exception as e:
+            await message.answer(f"Произошла ошибка при обработке файла: {str(e)}")
+        finally:
+            await state.clear()  # Очищаем состояние после завершения процесса
+
 
 
 
@@ -49,12 +68,25 @@ async def handle_new_file_inventory(message: Message, state: FSMContext, bot: Bo
     """
     data = await state.get_data()  # Получаем сохранённые данные из состояния
     period = datetime.strptime(data["period"],"%m.%Y").date()  # Получаем период из сохранённых данных и преобразуем его в объект date
-    try:
-        file = await bot.get_file(message.document.file_id)
-        await bot.download_file(file.file_path, destination="tmp/inventory.xlsx")
-        result = parse_inventory("tmp/inventory.xlsx", period)
-        await message.answer(f"Загружено записей: {len(result)}")
-    except Exception as e:
-        await message.answer(f"Произошла ошибка при обработке файла: {str(e)}")
-    finally:
-        await state.clear()
+    async with httpx.AsyncClient() as client:
+        try:
+            # Перед загрузкой нового файла удаляем все существующие записи инвентаря через API
+            response = await client.delete(f"{api_url}/api/inventories/")
+            response.raise_for_status()
+
+            file = await bot.get_file(message.document.file_id)
+            await bot.download_file(file.file_path, destination="tmp/inventory.xlsx")
+            result = parse_inventory("tmp/inventory.xlsx", period)
+
+            # Отправляем каждую запись инвентаря в API
+            for inventory in result:
+                # Преобразуем дату в строку в формате ISO, чтобы её можно было отправить в JSON
+                inventory["period"] = inventory["period"].isoformat()
+                response = await client.post(f"{api_url}/api/inventories/", json=inventory)
+                response.raise_for_status()
+
+            await message.answer(f"Загружено записей: {len(result)}")
+        except Exception as e:
+            await message.answer(f"Произошла ошибка при обработке файла: {str(e)}")
+        finally:
+            await state.clear()
