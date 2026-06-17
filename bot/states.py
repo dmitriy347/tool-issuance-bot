7469aaea4da5c9581cd0e36404d9d9a86368e7ba
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-06-17 16:26:30 +0500

    feat(bot/states): добавил состояние для генерации документа

--- bot/states.py
+++ bot/states.py
@@ -8,3 +8,6 @@ class UpdateInventory(StatesGroup):
     """Состояния для обновления инвентаря."""
     waiting_for_file = State()  # Ожидание загрузки файла пользователем
 
+class GenerateDocument(StatesGroup):
+    """Состояния для генерации документа."""
+    waiting_for_screen = State()  # Ожидание загрузки скриншота пользователем
