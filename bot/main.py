e88c0fa1bac494fce7d3c383e5713cdcbc2eb01d
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-06-19 17:24:31 +0500

    feat(bot/main): добавил  команды бота для автозаполнения меню

--- bot/main.py
+++ bot/main.py
@@ -2,6 +2,9 @@ import os
 from os import getenv
 from aiogram import Dispatcher, Bot
 import asyncio
+
+from aiogram.types import BotCommand
+
 from bot.handlers.commands import router as command_handler
 from bot.handlers.document import router as document_handler
 
@@ -13,6 +16,11 @@ dp.include_router(document_handler)
 
 async def main():
     os.makedirs("tmp", exist_ok=True)  # Создаём папку tmp, если её нет
+    await bot.set_my_commands([
+        BotCommand(command="update_directory", description="Загрузить справочник сотрудников"),
+        BotCommand(command="update_inventory", description="Загрузить инвентарь для нового месяца"),
+        BotCommand(command="generate", description="Сгенерировать документ по скриншоту"),
+    ])
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
